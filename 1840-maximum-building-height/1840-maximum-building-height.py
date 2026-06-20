5531735f417607f43702698b25a8ece75281a3eb
Dhaksin S <143109754+Dhaksin53@users.noreply.github.com>
2026-06-20 12:35:35 +0530

    Time: 251 ms (56.82%), Space: 46.4 MB (72.73%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
+        restrictions.append([1,0])
+        restrictions.append([n,n-1])
+        restrictions.sort()
+        m=len(restrictions)
+        for i in range(1,m):
+            dist=restrictions[i][0]-restrictions[i-1][0]
+            restrictions[i][1]=min(restrictions[i][1],restrictions[i-1][1]+dist)
+
+        for i in range(m-2,-1,-1):
+            dist=restrictions[i+1][0]-restrictions[i][0]
+            restrictions[i][1]=min(restrictions[i][1],restrictions[i+1][1]+dist)
+
+        ans=0
+        for i in range(1,m):
+            x1,h1=restrictions[i-1]
+            x2,h2=restrictions[i]
+            d=x2-x1
+            h=(h1+h2+d)//2
+            ans=max(h,ans)
+        return ans
